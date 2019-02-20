7ed73d594f408131a5678f85c8edcb06cdf3fda9
Fabio Elias <felias@Fabios-MacBook-Pro.local>
2019-02-20 01:21:28 -0300

    adding some details

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-'use strict'
 import App from './app'
 import React from 'react'
 import { render} from 'react-dom'
@@ -15,11 +14,9 @@ const renderApp = (NextApp) => {
 
 renderApp(App)
 
-// Verifica se está em dev e com o hot loader habilitado
 if (module.hot) {
-    console.log('Existe')
     module.hot.accept('./app', () => {
-        const NextApp = require('./app').default // O nextapp vai ser a próxima atualização do app. É preciso fazer o require porque não pode utilizar o import no meio do arquivo
+        const NextApp = require('./app').default
         renderApp(NextApp)
-    }) // Primeiro parametro vai receber a entrada da aplicação (arquivo principal) que vai ser atulizado ou ter outros components atualizados
+    })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,21 @@
-'use strict'
-
 const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
     mode: 'production',
     performance: { hints: false },
-    devtool: 'source-map', // Serve para debugar. Lembrando que o index precisa utilizar o STATIC
+    devtool: 'source-map',
     entry: [
         'react-hot-loader/patch',
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
-        path.join(__dirname, 'src', 'index')  // Aqui usa o index.js (PS: Obrigatoriamente precisa ser .js)
+        path.join(__dirname, 'src', 'index')
 
     ],
     output: { 
-        path: path.join(__dirname, 'dist'), // Define o caminho
-        filename: 'bundle.js', // Da um nome para o arquivo (que será utilizado no index.html)
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
         publicPath: '/static/'
-        // o local onde o WP dev server vai ficar assistindo nosso arquivo de desenvolvimento. Não gera um arquivo fisico e sim um virtual em memoria e o publicPath diz onde o arquivo vai ficar.
-        // esse diretorio static, se olharmos na estrutura do projeto, não existe, apenas está em memória e sendo utilizado no index.html
-
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin()
@@ -28,10 +23,10 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/, // faz o teste lógico de arquivo
-                exclude: /node_modules/, //excluir a pasta de node_modules, pois é 3rd party
-                include: /src/, // manda verificar tudo que termina com .js dentro da pasta src
-                use: ['babel-loader', 'eslint-loader'] // usando como loader, o babel loader que foi instalado via npm
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: /src/,
+                use: ['babel-loader', 'eslint-loader']
             }
         ]
     }
